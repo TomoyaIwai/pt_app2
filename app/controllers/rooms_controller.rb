1730d9abfc8f704cb30b438a175a8b5a4aaae8fb
TomoyaIwai <tomoyanbaby@gmail.com>
2022-06-06 20:00:49 +0900

    room詳細機能実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -7,9 +7,23 @@ class RoomsController < ApplicationController
   end
 
   def show
+    @room = Room.find(params[:id])
+  end
+
+  def create
+    @room = Room.new(room_params)
+    @room.user_id = current_user.id
+    @room.save
+    redirect_to room_path(@room)
   end
 
   def edit
-    
   end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:title, :content, :genre, :age, :purpose, :image)
+  end
+
 end
