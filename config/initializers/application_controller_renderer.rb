851ec3dab094ed543b67b8e519586b2c4964a93d
TomoyaIwai <tomoyanbaby@gmail.com>
2022-06-06 14:08:00 +0900

    room編集画面作成

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '016c5775d0b4207dcb691e2759ee8771f0f5e54cf54119c425f50bf727effc9f49838abc77e3aec57091348ec3d20765c6247901207d4df0d6f0ace46f9d3c7e'
