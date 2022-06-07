0306a0a9583b5480fa4b2b40890fdf9674116594
TomoyaIwai <tomoyanbaby@gmail.com>
2022-06-07 13:17:49 +0900

    room削除機能実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -28,6 +28,12 @@ class RoomsController < ApplicationController
     redirect_to room_path(@room)
   end
 
+  def destroy
+    @room = Room.find(params[:id])
+    @room.destroy
+    redirect_to rooms_path
+  end
+
   private
 
   def room_params
